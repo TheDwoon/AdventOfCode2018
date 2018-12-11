d2c721f2367b00ad53305cc0ff6b2cc9ccf9420e
TheDwoon <TheDwoon@gmail.com>
2018-12-11 22:18:17 +0100

    improved Day05 (simpler and faster)

--- src/main/java/com/github/thedwoon/aoc/Day05.java
+++ src/main/java/com/github/thedwoon/aoc/Day05.java
@@ -3,6 +3,7 @@ package com.github.thedwoon.aoc;
 import java.util.Arrays;
 
 public final class Day05 extends AbstractDay<String> {
+	private static final char NONE = '-';
 	
 	public Day05() {
 		super();
@@ -19,7 +20,7 @@ public final class Day05 extends AbstractDay<String> {
 	
 	@Override
 	protected String runPart1(String input) {
-		return Integer.toString(react(input).length());
+		return Integer.toString(react(input));
 	}
 	
 	@Override
@@ -31,31 +32,47 @@ public final class Day05 extends AbstractDay<String> {
 			String lower = new String( new char[] { Character.toLowerCase(alphabet[i]) });
 			String upper = new String( new char[] { Character.toUpperCase(alphabet[i]) });
 			String modified = input.replace(lower, "").replace(upper, "");
-			resultingLength[i] = react(modified).length();
+			resultingLength[i] = react(modified);
 		}
 		
 		return Integer.toString(Arrays.stream(resultingLength).min().orElse(0));
 	}
 	
-	private String react(String input) {
-		int size = 0;
-		do {
-			size = input.length();
-			
-			for (int i = 0; i < input.length() - 1; i++) {
-				char a = input.charAt(i);
-				char b = input.charAt(i + 1);
-				
-				if (Character.toLowerCase(a) == Character.toLowerCase(b) && a != b) {
-					if (i + 2 < input.length()) {
-						input = input.substring(0, i) + input.substring(i + 2);
-					} else {
-						input = input.substring(0, i);
-					}
-				}
-			}
-		} while (input.length() != size);
+	private int react(String input) {
+		char[] buffer = input.toCharArray();
+		int currentIndex = 0;
+		int currentNext = 1;
+		int size = buffer.length;
 		
-		return input;
+		while (currentNext != -1) {
+			char a = buffer[currentIndex];
+			char b = buffer[currentNext];
+			if (Character.toLowerCase(a) == Character.toLowerCase(b) && a != b) {
+				buffer[currentIndex] = NONE;
+				buffer[currentNext] = NONE;
+				size -= 2;
+				
+				int prevIndex = prevIndex(buffer, currentIndex);
+				currentIndex = prevIndex != -1 ? prevIndex : nextIndex(buffer, currentIndex);
+				currentNext = nextIndex(buffer, currentNext);
+			} else {
+				currentIndex = currentNext;
+				currentNext = nextIndex(buffer, currentNext);
+			}
+		}
+		
+		return size;
+	}
+	
+	private int nextIndex(char[] buffer, int currentIndex) {
+		while (++currentIndex < buffer.length && buffer[currentIndex] == NONE);
+		
+		return currentIndex < buffer.length ? currentIndex : -1;
+	}
+	
+	private int prevIndex(char[] buffer, int currentIndex) {
+		while (--currentIndex >= 0 && buffer[currentIndex] == NONE);
+		
+		return currentIndex >= 0 ? currentIndex : -1;
 	}
 }
