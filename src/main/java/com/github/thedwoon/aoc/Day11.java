fb2942e21e379ea8731e17cbbd926e3424ee3738
TheDwoon <TheDwoon@gmail.com>
2018-12-12 00:40:12 +0100

    improved Day11

--- src/main/java/com/github/thedwoon/aoc/Day11.java
+++ src/main/java/com/github/thedwoon/aoc/Day11.java
@@ -44,33 +44,19 @@ public final class Day11 extends AbstractDay<Integer> {
 		return String.format("(%3d,%3d)", bestX + 1, bestY + 1);
 	}
 
-	private static int computeCluster(int[][] grid, int x, int y) {
-		int sum = 0;
-		for (int i = x; i < x + 3; i++) {
-			for (int j = y; j < y + 3; j++) {
-				sum += grid[i][j];
-			}
-		}
-		
-		return sum;
-	}
-	
 	@Override
 	protected String runPart2(Integer serial) {
-		final List<Future<BurnWitchBurn>> futures = new LinkedList<>();
+		final List<Future<SmartWitchSmart>> futures = new LinkedList<>();
 		final int[][] grid = getPowerGrid(serial);
-		
-		ExecutorService executorService = Executors.newFixedThreadPool(64);
+		final int[][] summedAreaTable = computeSummedAreaTable(grid);
+				
+		ExecutorService executorService = Executors.newFixedThreadPool(16);
 		
 		for (int x = 0; x < GRID_SIZE - 1; x++) {
-			for (int y = 0; y < GRID_SIZE - 1; y++) {
-				for (int width = 1; width < GRID_SIZE - Math.max(x, y); width++) {
-					futures.add(executorService.submit(new BurnWitchBurn(grid, x, y, width)));
-				}
-			}
+			futures.add(executorService.submit(new SmartWitchSmart(summedAreaTable, x)));
 		}
 		
-		BurnWitchBurn best = futures.stream().map(t -> {
+		SmartWitchSmart best = futures.stream().map(t -> {
 			try {
 				return t.get();
 			} catch (InterruptedException | ExecutionException e) {
@@ -84,6 +70,54 @@ public final class Day11 extends AbstractDay<Integer> {
 		return best == null ? null : best.toString();
 	}
 
+	private static int computeCluster(int[][] grid, int x, int y) {
+		int sum = 0;
+		for (int i = x; i < x + 3; i++) {
+			for (int j = y; j < y + 3; j++) {
+				sum += grid[i][j];
+			}
+		}
+		
+		return sum;
+	}
+
+	private static int[][] computeSummedAreaTable(int[][] grid) {
+		int[][] summedAreaTable = new int[GRID_SIZE][GRID_SIZE];
+		
+		for (int x = 0; x < GRID_SIZE; x++) {			
+			for (int y = 0; y < GRID_SIZE; y++) {
+				summedAreaTable[x][y] = grid[x][y];
+				
+				if (x > 0)
+					summedAreaTable[x][y] += summedAreaTable[x - 1][y];
+				if (y > 0) 
+					summedAreaTable[x][y] += summedAreaTable[x][y - 1];
+				if (x > 0 && y > 0) 
+					summedAreaTable[x][y] -= summedAreaTable[x - 1][y - 1];
+			}
+		}
+		
+		return summedAreaTable;
+	}
+	
+	private static int computeArea(int[][] summedAreaTable, int x, int y, int width) {
+		int sum = 0;
+		final int maxX = x + width;
+		final int maxY = y + width;
+		
+		if (x > 0 && y > 0)
+			sum += summedAreaTable[x - 1][y - 1];
+		
+		sum += summedAreaTable[maxX - 1][maxY - 1];
+		
+		if (x > 0) 
+			sum -= summedAreaTable[x - 1][maxY - 1];
+		if (y > 0)
+			sum -= summedAreaTable[maxX - 1][y - 1];
+		
+		return sum;
+	}
+	
 	private static int getPowerLevel(int x, int y, int serial) {
 		final int rackId = x + 11;
 		return (rackId * (y + 1) + serial) * rackId / 100 % 10 - 5;
@@ -101,7 +135,7 @@ public final class Day11 extends AbstractDay<Integer> {
 		return grid;
 	}
 	
-	private static class BurnWitchBurn implements Callable<BurnWitchBurn> {
+	public static class BurnWitchBurn implements Callable<BurnWitchBurn> {
 		private final int[][] grid;
 		private final int minX;
 		private final int maxX;
@@ -132,6 +166,49 @@ public final class Day11 extends AbstractDay<Integer> {
 		@Override
 		public String toString() {
 			return String.format("(%3d,%3d): %d - %d", minX + 1, minY + 1, (maxX - minX), sum);
+		}
+	}
+	
+	public static class SmartWitchSmart implements Callable<SmartWitchSmart> {
+		private final int[][] summedAreaTable;
+
+		private final int x;
+		private int y;		
+		private int width;
+		private int sum = 0;
+		
+		private SmartWitchSmart(int[][] summedAreaTable, int x) {
+			this.summedAreaTable = summedAreaTable;
+			this.x = x;
+			this.y = 0;
+		}
+
+		@Override
+		public SmartWitchSmart call() throws Exception {
+			int bestArea = Integer.MIN_VALUE;
+			int bestY = -1;
+			int bestWidth = -1;
+			for (int y = 0; y < GRID_SIZE - 1; y++) {
+				for (int width = 1; width < GRID_SIZE - Math.max(this.x, y); width++) {
+					int area = computeArea(summedAreaTable, this.x, y, width);
+					if (area > bestArea) {
+						bestArea = area;
+						bestY = y;
+						bestWidth = width;
+					}
+				}
+			}
+			
+			this.y = bestY;
+			this.width = bestWidth;
+			this.sum = bestArea;
+			
+			return this;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("(%3d,%3d): %d - %d", x + 1, y + 1, width, sum);
 		}
 	}
 }
