0507eb72cb6ce11c917f2b87ba1a39f90883766f
TheDwoon <TheDwoon@gmail.com>
2018-12-11 21:37:15 +0100

    improved Day09 with a rotating list

--- src/main/java/com/github/thedwoon/aoc/Day09.java
+++ src/main/java/com/github/thedwoon/aoc/Day09.java
@@ -1,7 +1,6 @@
 package com.github.thedwoon.aoc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,25 +31,24 @@ public final class Day09 extends AbstractDay<int[]> {
 		Game g = new Game();
 		
 		for (int marble = 1; marble <= rounds; marble++) {
-			if (marble % (rounds / 10) == 0)
-				System.out.printf("PROGRESS: %.2f\n", marble / (double) rounds);
+//			if (marble % (rounds / 10) == 0)
+//				System.out.printf("PROGRESS: %.2f\n", marble / (double) rounds);
 			int player = (marble - 1) % playerScores.length; 
 			playerScores[player] += g.placeMarble(marble);
-//			System.out.println(g);
 		}
 		
 		long maxScore = playerScores[0];
-		int bestPlayer = 0;
+//		int bestPlayer = 0;
 		for (int p = 0; p < playerScores.length; p++) {
 			if (playerScores[p] > maxScore) {
-				bestPlayer = p;
+//				bestPlayer = p;
 				maxScore = playerScores[p];
 			}
 			
-			System.out.printf("Score for Player %d: %d\n", p + 1, playerScores[p]);
+//			System.out.printf("Score for Player %d: %d\n", p + 1, playerScores[p]);
 		}
 		
-		System.out.printf("***** WINNER *****\nPlayer %d: %d\n", bestPlayer + 1, maxScore);
+//		System.out.printf("***** WINNER *****\nPlayer %d: %d\n", bestPlayer + 1, maxScore);
 		return Long.toString(maxScore);
 	}
 	
@@ -59,41 +57,21 @@ public final class Day09 extends AbstractDay<int[]> {
 	}
 
 	private class Game {
-		private final List<Integer> marbles = new ArrayList<>(3_500_000);
-		private int currentMarbleIndex = 0;
+		private final CircularList marbles = new CircularList();
 		
 		private Game() {
-			marbles.add(0);
+
 		}
 		
 		public int placeMarble(int marbleNumber) {
 			if (marbleNumber % 23 == 0) {
-				currentMarbleIndex -= 7;
-				while (currentMarbleIndex < 0)
-					currentMarbleIndex += marbles.size();
-				
-				int marbleRemoved = marbles.remove(currentMarbleIndex);
-				currentMarbleIndex %= marbles.size();
+				marbles.shiftLeft(7);
+				int marbleRemoved = marbles.removeHead();
 				
 				return marbleNumber + marbleRemoved;
 			} else {
-				int index1R = (currentMarbleIndex + 1) % marbles.size();
-				int index2R = (currentMarbleIndex + 2) % marbles.size();
-				
-				int insertionPoint;
-				if (index1R == 0 && index2R == 0) {
-					// first element, simply append
-					insertionPoint = marbles.size();
-				} else if (index1R == marbles.size() - 1 && index2R == 0) {
-					// append at the end
-					insertionPoint = marbles.size();
-				} else {
-					insertionPoint = index2R;
-				}
-				
-				marbles.add(insertionPoint, marbleNumber);
-				currentMarbleIndex = insertionPoint;
-				
+				marbles.shiftRight(1);				
+				marbles.addRight(marbleNumber);
 				return 0;
 			}
 		}
@@ -105,13 +83,13 @@ public final class Day09 extends AbstractDay<int[]> {
 			sb.append('[');
 			int i = 0;
 			for (int marble : marbles) {
-				if (i == currentMarbleIndex) {
+				if (i == 0) {
 					sb.append('(');
 				}
 				
 				sb.append(Integer.toString(marble));
 				
-				if (i == currentMarbleIndex) {
+				if (i == 0) {
 					sb.append(')');
 				}
 				
@@ -124,6 +102,119 @@ public final class Day09 extends AbstractDay<int[]> {
 			sb.append(']');
 			
 			return sb.toString();
+		}
+	}
+	
+	private static class CircularList implements Iterable<Integer> {
+		private ListNode head;
+		private int size = 1;
+		
+		private CircularList() {
+			head = new ListNode(0);
+			head.next = head;
+			head.prev = head;
+		}
+		
+		public void addRight(int value) {
+			ListNode node = new ListNode(value);
+
+			ListNode nextNode = head.next;
+			ListNode prevNode = head;
+			
+			node.prev = prevNode;
+			node.next = nextNode;
+			
+			nextNode.prev = node;
+			prevNode.next = node;
+			
+			head = node;
+			size += 1;
+		}
+		
+		public void shiftRight(int amount) {
+			for (int i = 0; i < amount; i++)
+				head = head.next;
+		}
+		
+		public void shiftLeft(int amount) {
+			for (int i = 0; i < amount; i++)
+				head = head.prev;
+		}
+		
+		@SuppressWarnings("unused")
+		public int getHead() {
+			return head.value;
+		}
+		
+		public int removeHead() {
+			if (head.next == head)
+				throw new IllegalStateException("one shall not remove the last element");
+			
+			ListNode removed = head;
+			head = removed.next;
+			
+			removed.prev.next = removed.next;
+			removed.next.prev = removed.prev;
+			
+			size -= 1;
+			
+			return removed.value;
+		}
+		
+		public int size() {
+			return size;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append('[');
+			final ListNode limiter = head;
+			ListNode current = head;
+			do {
+				sb.append(current.value);
+				if (current.next != limiter)
+					sb.append(", ");
+				
+				current = current.next;
+			} while (current != limiter);
+			sb.append(']');
+			
+			return sb.toString();
+		}
+		
+		@Override
+		public Iterator<Integer> iterator() {
+			return new Iterator<Integer>() {
+				private final ListNode limiter = head;
+				private ListNode current = null;
+				
+				@Override
+				public boolean hasNext() {
+					return current == null || current.next != limiter;
+				}
+
+				@Override
+				public Integer next() {
+					if (current == null)
+						current = head;
+					else
+						current = current.next;
+					
+					return current.value;
+				}
+			};
+		}
+	}
+	
+	private static class ListNode {
+		private final int value;
+
+		private ListNode next;
+		private ListNode prev;
+	
+		private ListNode(int value) {
+			this.value = value;
 		}
 	}
 }
